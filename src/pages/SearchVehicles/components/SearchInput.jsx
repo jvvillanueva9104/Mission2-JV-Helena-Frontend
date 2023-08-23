da5b8c625c843639b904ad990570e3641f56ef42
JV Villanueva <jan@missionreadyhq.com>
2023-08-23 14:48:34 +1200

    created function to convert uploaded image to binary(base64) data url

--- src/pages/SearchVehicles/components/SearchInput.jsx
+++ src/pages/SearchVehicles/components/SearchInput.jsx
@@ -4,6 +4,7 @@ import { useState } from "react";
 
 const SearchInput = () => {
   const [uploadedImage, setUploadedImage] = useState(null);
+  const [isClicked, setIsClicked] = useState(false);
 
   const handleFileChange = (event) => {
     const file = event.target.files[0];
@@ -12,47 +13,67 @@ const SearchInput = () => {
       reader.onload = (e) => {
         setUploadedImage(e.target.result);
       };
+
       reader.readAsDataURL(file);
     }
   };
 
+  console.log(uploadedImage);
+
   const handleRemoveImage = () => {
     setUploadedImage(null);
   };
+
+  const handleUploadClick = () => {
+    setIsClicked(true);
+  };
+
   return (
     <div className="searchInputContainer">
-      <div>
-        <h1 className="searchHeader">Find The Car Of Your Dreams</h1>
-        <div className="fileUploadContainer">
-          {uploadedImage && (
-            <div>
-              <img
-                data-aos="zoom-in"
-                src={uploadedImage}
-                alt="Uploaded"
-                className="uploadedImage"
-              />
-              <button className="removeBtn" onClick={handleRemoveImage}>
-                x
+      {!isClicked ? (
+        <div>
+          <h1 className="searchHeader">Find The Car Of Your Dreams</h1>
+          <div className="fileUploadContainer">
+            {uploadedImage && (
+              <div>
+                <img
+                  data-aos="zoom-in"
+                  src={uploadedImage}
+                  alt="Uploaded"
+                  className="uploadedImage"
+                />
+                <button className="removeBtn" onClick={handleRemoveImage}>
+                  x
+                </button>
+              </div>
+            )}
+            <input
+              className="fileUpload"
+              type="file"
+              onChange={handleFileChange}
+            />
+            {uploadedImage && (
+              <button className="uploadBtn" onClick={handleUploadClick}>
+                Upload
               </button>
-            </div>
-          )}
-          <input
-            className="fileUpload"
-            type="file"
-            onChange={handleFileChange}
-          />
-          {uploadedImage && <button className="uploadBtn">Upload</button>}
+            )}
+          </div>
         </div>
-      </div>
-
-      {/* <iframe
-        title="Turners Search Results"
-        src="https://www.turners.co.nz/Cars/Used-Cars-for-Sale/audi/?sortorder=7&pagesize=20&pageno=1&issearchsimilar=true&types=hatchback&make=audi"
-        width="100%"
-        height="600%"
-        frameborder="0"
-      ></iframe> */}
+      ) : (
+        <>
+          {isClicked && (
+            <iframe
+              title="Turners Search Results"
+              src="https://www.turners.co.nz/Cars/Used-Cars-for-Sale/audi/?sortorder=7&pagesize=20&pageno=1&issearchsimilar=true&types=hatchback&make=audi"
+              width="100%"
+              height="733"
+              frameborder="0"
+              loading="lazy"
+              className="iFrame"
+            ></iframe>
+          )}
+        </>
+      )}
     </div>
   );
 };
